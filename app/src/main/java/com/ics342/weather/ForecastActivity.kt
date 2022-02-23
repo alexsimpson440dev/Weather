a813e8262e2a18aa5f2719a8a154e0b73741e0c1
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-02-22 18:55:00 -0600

    updated forecast activity to use a view model

--- app/src/main/java/com/ics342/weather/ForecastActivity.kt
+++ app/src/main/java/com/ics342/weather/ForecastActivity.kt
@@ -4,22 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.ics342.weather.domains.Forecast
 import com.ics342.weather.domains.DayForecast
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import com.ics342.weather.domains.Forecast
+import com.ics342.weather.viewmodels.ForecastViewModel
 
 class ForecastActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private val viewModel = ForecastViewModel()
     private var adapterData = mutableListOf<DayForecast>()
     private lateinit var adapter: ForecastAdapter
-    private lateinit var api: Api
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,37 +23,14 @@ class ForecastActivity : AppCompatActivity() {
         adapter = ForecastAdapter(adapterData)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org/data/2.5/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-
-        api = retrofit.create(Api::class.java)
     }
 
     override fun onResume() {
         super.onResume()
-        val call: Call<Forecast> = api.getForecast("55304")
-        call.enqueue(object : Callback<Forecast> {
-            override fun onResponse(
-                call: Call<Forecast>,
-                response: Response<Forecast>
-            ) {
-                val forecasts = response.body()
-                forecasts?.let {
-                    bindData(it)
-                }
-            }
-
-            override fun onFailure(call: Call<Forecast>, t: Throwable) {
-                println("An error occurred due to: $t")
-            }
-        })
+        viewModel.forecast.observe(this) { forecast ->
+            bindData(forecast)
+        }
+        viewModel.loadData()
     }
 
     private fun bindData(forecast: Forecast) {
---
+++ app/src/main/java/com/ics342/weather/viewmodels/ForecastViewModel.kt
@@ -0,0 +1,46 @@
+package com.ics342.weather.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import com.ics342.weather.Api
+import com.ics342.weather.domains.Forecast
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class ForecastViewModel {
+
+    val forecast: MutableLiveData<Forecast> = MutableLiveData()
+
+    fun loadData() {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        val service = retrofit.create(Api::class.java)
+        val call: Call<Forecast> = service.getForecast("55304")
+        call.enqueue(object : Callback<Forecast> {
+            override fun onResponse(
+                call: Call<Forecast>,
+                response: Response<Forecast>
+            ) {
+
+                response.body()?.let {
+                    forecast.value = it
+                }
+            }
+
+            override fun onFailure(call: Call<Forecast>, t: Throwable) {
+                println("An error occurred due to: $t")
+            }
+        })
+    }
+}
