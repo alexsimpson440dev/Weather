e1290cf43b3351029acbd0f34e3ff327b6f0c226
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-03-13 15:06:27 -0500

    current conditions fragment works now

--- app/src/main/java/com/ics342/weather/fragments/CurrentConditionsFragment.kt
+++ app/src/main/java/com/ics342/weather/fragments/CurrentConditionsFragment.kt
@@ -1,12 +1,59 @@
 package com.ics342.weather.fragments
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.ics342.weather.R
+import com.ics342.weather.databinding.CurrentConditionsFragmentBinding
+import com.ics342.weather.domains.CurrentConditions
+import com.ics342.weather.viewmodels.CurrentConditionsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class CurrentConditionsFragment : Fragment() {
+@AndroidEntryPoint
+class CurrentConditionsFragment : Fragment(R.layout.current_conditions_fragment) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: CurrentConditionsFragmentBinding
+    @Inject lateinit var viewModel: CurrentConditionsViewModel
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = CurrentConditionsFragmentBinding.bind(view)
+
+//        binding.forecastButton.setOnClickListener {
+//            // todo: start the next fragment transaction here
+//
+////            startActivity(Intent(this, ForecastActivity::class.java))
+////            supportFragmentManager.beginTransaction().apply {
+////                replace(R.id.flFragment, currentConditionsFragment)
+////                commit()
+////            }
+//        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.currentConditions.observe(this) { currentConditions ->
+            bindData(currentConditions)
+        }
+        viewModel.loadData()
+    }
+
+    private fun bindData(currentConditions: CurrentConditions) {
+        binding.locationName.text = currentConditions.name
+        binding.temperature.text = getString(R.string.temperature, currentConditions.main.temp)
+        binding.feelsLike.text = getString(R.string.feels_like, currentConditions.main.feelsLike)
+        binding.low.text = getString(R.string.low, currentConditions.main.tempMin)
+        binding.high.text = getString(R.string.high, currentConditions.main.tempMax)
+        binding.humidity.text = getString(R.string.humidity, currentConditions.main.humidity)
+        binding.pressure.text = getString(R.string.pressure, currentConditions.main.pressure)
+
+        val iconName = currentConditions.weather.firstOrNull()?.icon
+        val iconUrl = "https://openweathermap.org/img/wn/$iconName@2x.png"
+        Glide.with(this)
+            .load(iconUrl)
+            .into(binding.conditionIcon)
     }
 }
--- app/src/main/java/com/ics342/weather/viewmodels/CurrentConditionsViewModel.kt
+++ app/src/main/java/com/ics342/weather/viewmodels/CurrentConditionsViewModel.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(private val service: Api): ViewModel() {
+class CurrentConditionsViewModel @Inject constructor(private val service: Api): ViewModel() {
 
     private val _currentConditions = MutableLiveData<CurrentConditions>()
     val currentConditions: LiveData<CurrentConditions>
