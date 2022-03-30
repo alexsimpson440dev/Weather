4bd2198a26d63d1268495b8620d83753f1759cfa
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-03-30 00:07:43 -0500

    added location permissions

--- app/src/main/java/com/ics342/weather/fragments/SearchFragment.kt
+++ app/src/main/java/com/ics342/weather/fragments/SearchFragment.kt
@@ -1,11 +1,22 @@
 package com.ics342.weather.fragments
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.ics342.weather.R
 import com.ics342.weather.databinding.FragmentSearchBinding
 import com.ics342.weather.domains.CurrentConditions
@@ -18,6 +29,17 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private lateinit var binding: FragmentSearchBinding
     @Inject lateinit var viewModel: SearchViewModel
+    private lateinit var locationProviderClient: FusedLocationProviderClient
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Log.i("permission", "granted")
+        } else {
+            Log.i("permission: ", "not granted")
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -50,6 +72,10 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
                 navigateToCurrentConditions(currentConditions)
             }
         }
+
+        binding.getLocationButton.setOnClickListener {
+            requestPermission(requireContext(), requireActivity())
+        }
     }
 
     private fun navigateToCurrentConditions(currentConditions: CurrentConditions) {
@@ -60,5 +86,29 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         )
 
         findNavController().navigate(action)
+    }
+
+    private fun requestPermission(context: Context, activity: Activity) {
+        when {
+            ContextCompat.checkSelfPermission(
+                context,
+                ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("Permission", "granted")
+                locationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+                locationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+                    Log.i("Location", location.toString())
+                }
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                activity,
+                ACCESS_COARSE_LOCATION
+            ) -> {
+                Log.i("Permission", "permission not accepted yet")
+            }
+            else -> {
+                requestPermissionLauncher.launch(ACCESS_COARSE_LOCATION)
+            }
+        }
     }
 }
