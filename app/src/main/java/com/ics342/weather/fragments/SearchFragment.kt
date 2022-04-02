c8a965ee3392cd2353e9bd601192fd9031e573f4
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-04-02 00:32:59 -0500

    removed unused import

--- app/src/main/java/com/ics342/weather/fragments/SearchFragment.kt
+++ app/src/main/java/com/ics342/weather/fragments/SearchFragment.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
