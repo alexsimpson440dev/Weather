0d976f23620e1cc492ca38344e04838568466d7f
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-04-02 00:36:56 -0500

    removed activity code because this is done in a fragment

--- app/src/main/java/com/ics342/weather/MainActivity.kt
+++ app/src/main/java/com/ics342/weather/MainActivity.kt
@@ -2,11 +2,10 @@ package com.ics342.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
